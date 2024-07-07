357e607c9baba4033b772d5780d97802f001e44b
Adri <Adri@DESKTOP-GH5CEIL>
2024-07-07 23:25:57 +0200

    Commit Test

--- ecommerceApiLauncher/src/test/java/com/ecommerce/api/ecommerceApiLauncher/EcommerceApiLauncherApplicationTests.java
+++ ecommerceApiLauncher/src/test/java/com/ecommerce/api/ecommerceApiLauncher/EcommerceApiLauncherApplicationTests.java
@@ -2,6 +2,20 @@ package com.ecommerce.api.ecommerceApiLauncher;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+
+import com.ecommerce.api.ecommerceApiLauncher.controller.*;
+import com.ecommerce.api.ecommerceApiLauncher.service.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 class EcommerceApiLauncherApplicationTests {
@@ -9,5 +23,17 @@ class EcommerceApiLauncherApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+	   @Autowired
+	    private MockMvc mockMvc;
 
+	    @MockBean
+	    private RateService service;
+
+	    @Test
+	    public void testGetRates() throws Exception {
+	        
+	        given(service.findAll()).willReturn(List.of());
+	        mockMvc.perform(get("/all"))
+	                .andExpect(status().isOk());
+	    }
 }
